33b0f8b147658d13d0b13bd4093d1c5981a944f5
Noé <110814367+NoeFarese@users.noreply.github.com>
2024-11-04 11:21:41 +0100

    update aufgabe 2.14

--- aufgaben_racket/p84_aufgabe_2.14.rkt
+++ aufgaben_racket/p84_aufgabe_2.14.rkt
@@ -1,7 +1,30 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname p84_aufgabe_2.14) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
-(: nummer->position (natural -> string))
+; Eine Position ist eine der folgenden:
+; - "Torwärtin"
+; - "Abwehrspielerin"
+; - "Mittelfeldspielerin"
+; - "Stürmerin"
+; - "Ersatzspielerin"
+; - "Ungültige Nummer"
+(define position
+  (signature (one-of "Torwärtin"
+                     "Abwehrspielerin"
+                     "Mittelfeldspielerin"
+                     "Stürmerin"
+                     "Ersatzspielerin"
+                     "Ungültige Nummer")))
+
+
+(: nummer->position (natural -> position))
+
+(check-expect (nummer->position 1) "Torwärtin")
+(check-expect (nummer->position 4) "Abwehrspielerin")
+(check-expect (nummer->position 7) "Mittelfeldspielerin")
+(check-expect (nummer->position 9) "Stürmerin")
+(check-expect (nummer->position 15) "Ersatzspielerin")
+(check-expect (nummer->position 100) "Ungültige Nummer")
 
 (define nummer->position
   (lambda (nummer)
@@ -12,10 +35,3 @@
       ((or (= nummer 9) (= nummer 11)) "Stürmerin")
       ((and (>= nummer 12) (<= nummer 99)) "Ersatzspielerin")
       (else "Ungültige Nummer"))))
-
-(check-expect (nummer->position 1) "Torwärtin")
-(check-expect (nummer->position 4) "Abwehrspielerin")
-(check-expect (nummer->position 7) "Mittelfeldspielerin")
-(check-expect (nummer->position 9) "Stürmerin")
-(check-expect (nummer->position 15) "Ersatzspielerin")
-(check-expect (nummer->position 100) "Ungültige Nummer")
